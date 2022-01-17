5d77619c57519335d98d5ec73043de90bdb12a8e
Krat Verma <krat.verma@intelliswift.com>
2022-01-17 18:09:11 +0530

    removed

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,41 +1,41 @@
-package com.example.demo.student;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
-import static java.time.Month.*;
-
-@Configuration
-public class StudentConfig {
-
-    @Bean
-    CommandLineRunner commandLineRunner(StudentRepository repository ){
-        return args -> {
-            Student krat = new Student(
-                    "Krat",
-                    LocalDate.of(1998, APRIL, 23),
-                    "krat.test@gmail.com"
-            );
-            Student anuj = new Student(
-                    "Anuj",
-                    LocalDate.of(1999, OCTOBER, 15),
-                    "anuj.test@gmail.com"
-            );
-
-            Student yesh = new Student(
-                    "Yesh",
-                    LocalDate.of(2000, APRIL, 10),
-                    "yesh.test@gmail.com"
-            );
-
-            repository.saveAll(
-                    List.of(krat,anuj,yesh)
-            );
-        };
-    }
-}
+//package com.example.demo.student;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.time.LocalDate;
+//import java.time.Month;
+//import java.util.List;
+//
+//import static java.time.Month.*;
+//
+//@Configuration
+//public class StudentConfig {
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository repository ){
+//        return args -> {
+//            Student krat = new Student(
+//                    "Krat",
+//                    LocalDate.of(1998, APRIL, 23),
+//                    "krat.test@gmail.com"
+//            );
+//            Student anuj = new Student(
+//                    "Anuj",
+//                    LocalDate.of(1999, OCTOBER, 15),
+//                    "anuj.test@gmail.com"
+//            );
+//
+//            Student yesh = new Student(
+//                    "Yesh",
+//                    LocalDate.of(2000, APRIL, 10),
+//                    "yesh.test@gmail.com"
+//            );
+//
+//            repository.saveAll(
+//                    List.of(krat,anuj,yesh)
+//            );
+//        };
+//    }
+//}
