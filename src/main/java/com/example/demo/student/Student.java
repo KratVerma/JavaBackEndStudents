94c6a20052eaeab6e15b70ee3f416f14a8a45702
Krat Verma <krat.verma@intelliswift.com>
2022-01-19 15:23:46 +0530

    lombok used

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,11 +1,15 @@
 package com.example.demo.student;
 
-import org.apache.tomcat.jni.Local;
+import lombok.*;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.Period;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 @Table
 public class Student {
@@ -21,76 +25,6 @@ public class Student {
     )
     private Long id;
     private String name;
-    private LocalDate dob;
+    private Long phoneNumber;
     private String email;
-
-    @Transient
-    private Integer age;
-
-    public Student() {
-    }
-
-    public Student(Long id, String name, LocalDate dob, String email) {
-        this.id = id;
-        this.name = name;
-        this.dob = dob;
-        this.email = email;
-    }
-
-    public Student(String name, LocalDate dob, String email) {
-        this.name = name;
-        this.dob = dob;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getAge() {
-        return Period.between(this.dob, LocalDate.now()).getYears();
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", dob=" + dob +
-                ", email='" + email + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
